a96ef2b547338ee1bbc8e6e0d4eceedd03568255
Jeremy Biddle <beeduul@gmail.com>
2014-01-18 10:28:11 -0800

    Initial Commit

---
+++ ParticlesOSX/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  ParticlesOSX
+//
+//  Created by Jeremy Biddle on 1/18/14.
+//  Copyright (c) 2014 beeduul. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ ParticlesOSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ParticlesOSX
+//
+//  Created by Jeremy Biddle on 1/18/14.
+//  Copyright (c) 2014 beeduul. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
